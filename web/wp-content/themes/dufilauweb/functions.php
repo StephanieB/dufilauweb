572116badd08c884464cf08a34832ebc7f195c0a
StephanieB <bellone.stephanie@gmail.com>
2016-05-10 20:06:02 +0200

    add custom post type and custom taxonomy

--- web/wp-content/themes/dufilauweb/functions.php
+++ web/wp-content/themes/dufilauweb/functions.php
@@ -32,3 +32,35 @@ add_action( 'widgets_init', 'theme_widgets_init' );
 register_nav_menus([
     'primary menu' => 'Main menu'
 ]);
+
+// Add custom post type
+function theme_create_custom_post_types() {
+    register_post_type('dufilauweb_folder', [
+        'labels' => [
+            'name' => __('Dossiers', 'dufilauweb'),
+            'singular_name' => __('Dossier', 'dufilauweb')
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'show_ui' => true,
+        'menu_position' => 21,
+        'menu_icon' => 'dashicons-book-alt',
+        'supports' => [
+            'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'revisions', 'post-formats'
+        ]
+    ]);
+}
+add_action('init', 'theme_create_custom_post_types');
+
+// Add custom taxonomy
+function theme_create_custom_taxonomies() {
+    register_taxonomy('dufilauweb_subject', 'dufilauweb_folder', [
+        'labels' => [
+            'name' => __('Sujets', 'dufilauweb'),
+            'singular_name' => __('Sujet', 'dufilauweb')
+        ],
+        'public' => true,
+        'hierarchical' => true
+    ]);
+}
+add_action('init', 'theme_create_custom_taxonomies');
