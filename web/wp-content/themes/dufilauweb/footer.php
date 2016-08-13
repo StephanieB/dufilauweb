7ed451898141b9eb5f0c8cc659948edb497a3dd1
StephanieB <bellone.stephanie@gmail.com>
2016-08-13 19:23:48 +0200

    add basic structure of the theme

--- web/wp-content/themes/dufilauweb/footer.php
+++ web/wp-content/themes/dufilauweb/footer.php
@@ -1,3 +1,11 @@
+<footer>
+    <?php
+        wp_nav_menu([
+            'theme_location' => 'footer menu',
+            'container' => 'nav'
+        ]);
+    ?>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
--- web/wp-content/themes/dufilauweb/functions.php
+++ web/wp-content/themes/dufilauweb/functions.php
@@ -30,7 +30,8 @@ add_action( 'widgets_init', 'theme_widgets_init' );
 
 // enabling menus support
 register_nav_menus([
-    'primary menu' => 'Main menu'
+    'primary menu' => __('Menu principal', 'dufilauweb'),
+    'footer menu' => __('Menu du pied de page', 'dufilauweb')
 ]);
 
 // Add custom post type
@@ -47,6 +48,9 @@ function theme_create_custom_post_types() {
         'menu_icon' => 'dashicons-book-alt',
         'supports' => [
             'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'revisions', 'post-formats'
+        ],
+        'rewrite' => [
+            'slug' => __('dossier', 'dufilauweb')
         ]
     ]);
 }
@@ -64,3 +68,60 @@ function theme_create_custom_taxonomies() {
     ]);
 }
 add_action('init', 'theme_create_custom_taxonomies');
+
+// Add HTML5 theme support
+function add_html5_support_theme() {
+    add_theme_support( 'html5', array( 'search-form' ) );
+}
+add_action( 'after_setup_theme', 'add_html5_support_theme' );
+
+// Add one item in the footer menu
+function add_menu_item_theme ( $items, $args ) {
+    if ($args->theme_location == 'footer menu') {
+        $items = '<li>© '.__('2015', 'dufilauweb').'</li>'.$items;
+    }
+    return $items;
+}
+add_filter( 'wp_nav_menu_items', 'add_menu_item_theme', 10, 2 );
+
+// Remove titles in a text
+function delete_title($text) {
+    $text = preg_replace('#<h[1-6]>(.*)</h[1-6]>#', '', $text);
+    return $text;
+}
+
+// Built a customized extract
+function custom_excerpt($text, $limit = 20, $more = '...')
+{
+    $text = delete_title($text);
+    $text = wp_strip_all_tags(strip_shortcodes($text));
+    $e_text = explode(' ', $text);
+    if (count($e_text) > $limit) {
+        array_splice($e_text, $limit);
+        $text = implode(' ', $e_text).$more;
+    }
+    return $text;
+}
+
+// Add CSS and JS
+function theme_scripts() {
+    wp_enqueue_style( 'css', get_template_directory_uri().'/build/css/style.min.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+
+// Publication moment
+function moment($timestamp){
+	setlocale(LC_TIME, 'fr_FR');
+	$dif = time() - $timestamp;
+	if($dif < 60){
+		return sprintf(__('Il y a %d secondes', 'cfrt'), $dif);
+	}elseif( $dif < 3600){
+		$min = round($dif/60);
+		return sprintf(__('Il y a %d minutes', 'cfrt'), $min);
+	}elseif($dif <= 3600*5){
+		$h = round($dif/3600);
+		return  sprintf(__('Il y a %d heures', 'cfrt'), $h);
+	}else {
+		return strftime ('%A %e %B %G %H:%M', $timestamp);
+	}
+}
